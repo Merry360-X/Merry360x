299c786da3b3f2324f1b9a082085dee821afb2be
Merry360-X <Merry360x@yahoo.com>
2026-09-14 11:49:05 +0200

    fix(web): correct ternary syntax in PropertyDetails JSX

--- src/pages/PropertyDetails.tsx
+++ src/pages/PropertyDetails.tsx
@@ -1611,7 +1611,7 @@ export default function PropertyDetails() {
               </Link>
             </div>
           </div>
-        ) : (
+        ) : data ? (
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
             {/* Gallery + content */}
             <div className="lg:col-span-7 space-y-6">
